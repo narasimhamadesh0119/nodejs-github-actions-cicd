5435ad38ff7d54bfe64f940bfe1ecf28a427303e
narasimhamadesh <narasimha.m@nmit-solutions.com>
2026-08-17 16:28:34 +0530

    Improve Express API tests with Supertest

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+// const express = require("express");
+
+// const app = express();
+
+// const port = 3040;
+
+// app.get("/", (req, res) => {
+//   res.send("GitHub Actions are working fine");
+// });
+
+// app.listen(port, () => {
+//   console.log(`App is listening on port ${port}`);
+// });
+
+
+
 const express = require("express");
 
 const app = express();
@@ -8,7 +24,10 @@ app.get("/", (req, res) => {
   res.send("GitHub Actions are working fine");
 });
 
-app.listen(port, () => {
-  console.log(`App is listening on port ${port}`);
-});
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`App is listening on port ${port}`);
+  });
+}
 
+module.exports = app;
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,13 +1,26 @@
-const { exec } = require("child_process");
+// const { exec } = require("child_process");
 
-describe("Node.js Application Test", () => {
-  test("application should start successfully", (done) => {
-    const app = exec("node app.js");
+// describe("Node.js Application Test", () => {
+//   test("application should start successfully", (done) => {
+//     const app = exec("node app.js");
 
-    setTimeout(() => {
-      app.kill();
-      expect(true).toBe(true);
-      done();
-    }, 2000);
+//     setTimeout(() => {
+//       app.kill();
+//       expect(true).toBe(true);
+//       done();
+//     }, 2000);
+//   });
+// });
+
+
+const request = require("supertest");
+const app = require("../app");
+
+describe("GET /", () => {
+  test("should return GitHub Actions are working fine", async () => {
+    const response = await request(app).get("/");
+
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toBe("GitHub Actions are working fine");
   });
 });
