34f7240503e4fb0eb133f6ce86a82d4bb1d41c0e
narasimhamadesh <narasimha.m@nmit-solutions.com>
2026-08-18 16:03:02 +0530

    updated the test file

--- tests/app.test.js
+++ tests/app.test.js
@@ -13,14 +13,26 @@
 // });
 
 
+// const request = require("supertest");
+// const app = require("../app");
+
+// describe("GET /", () => {
+//   test("should return GitHub Actions are working fine", async () => {
+//     const response = await request(app).get("/");
+
+//     expect(response.statusCode).toBe(200);
+//     expect(response.text).toBe("GitHub Actions are working fine");
+//   });
+// });
+
+
 const request = require("supertest");
 const app = require("../app");
 
 describe("GET /", () => {
-  test("should return GitHub Actions are working fine", async () => {
+  test("should return status 200", async () => {
     const response = await request(app).get("/");
 
     expect(response.statusCode).toBe(200);
-    expect(response.text).toBe("GitHub Actions are working fine");
   });
 });
